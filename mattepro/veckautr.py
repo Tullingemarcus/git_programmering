8b08353f122335ce72315f8365491322beac70d9
unknown <marcus@tullingeskog.se>
2019-11-14 11:00:03 +0100

    ändringar

---
+++ mattepro/veckautr.py
@@ -0,0 +1,8 @@
+
+x = int(input("vilken vecka är det: "))
+
+y = (-1*x)**x/x**x
+
+svar = y + 1 + (x+(y*-1+1)/2)/2
+
+print(svar)
---
+++ python/random/communist.py
@@ -0,0 +1,24 @@
+import turtle
+
+soviet = turtle.Turtle()
+
+turtle.bgcolor("red")
+
+
+soviet.color("black")
+soviet.backward(200)
+soviet.left(90)
+soviet.forward(100)
+
+#soviet.beginfill()
+soviet.color("black")
+
+
+
+
+
+
+
+
+
+input()
